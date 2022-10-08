a7e0aaf89bd58b4e2b289d2aa40c7798ed39f207
D sapphire obelisk <95326843+highYhell0w@users.noreply.github.com>
2022-10-08 22:29:18 +0700

    Helper functions
    Added helper functions for usage in file "exception.cc"

--- code/userprog/ksyscall.h
+++ code/userprog/ksyscall.h
@@ -8,14 +8,99 @@
  *
  **************************************************************/
 
-#ifndef __USERPROG_KSYSCALL_H__
-#define __USERPROG_KSYSCALL_H__
+#ifndef __USERPROG_KSYSCALL_H__ 
+#define __USERPROG_KSYSCALL_H__ 
 
 #include "kernel.h"
-#include "main.h"
+#include "synchconsole.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-void SysHalt() { kernel->interrupt->Halt(); }
+#define MAX_LENGTH_INT 11
 
-int SysAdd(int op1, int op2) { return op1 + op2; }
+
+void SysHalt()
+{
+  kernel->interrupt->Halt();
+}
+
+
+int SysAdd(int op1, int op2)
+{
+  return op1 + op2;
+}
+
+/**/
+
+int SysReadNum()
+{
+  char numChar[MAX_LENGTH_INT + 1];
+  memset(numChar, 0, sizeof(numChar));
+  char temp = kernel->synchConsoleIn->GetChar();
+  int index = 0;
+
+  if (isdigit(temp) || temp == '-') {}
+  else
+    {
+      DEBUG(dbgSys, "Weird..." << '\n');
+      DEBUG(dbgSys, "temp: " << (int) temp << '\n');
+      return -1;
+    }
+  
+  while (temp != (char)10)
+  {
+    DEBUG(dbgSys, "temp: " << (int) temp << '\n');
+    numChar[index++] = temp;
+    if (index > MAX_LENGTH_INT)
+    {
+      if (numChar[0] == '-')
+      {
+        DEBUG(dbgSys, "Invalid input - invalid length" << '\n'); 
+        return INT32_MIN;
+      }
+      else
+      {
+        DEBUG(dbgSys, "Invalid input - invalid length" << '\n'); 
+        return INT32_MAX;
+      }
+    }
+    temp = kernel->synchConsoleIn->GetChar();
+  }
+
+  int num = 0;
+  sscanf(numChar, "%d", &num);
+  return num;
+}
+
+/**/
+
+void SysPrintNum(int num)
+{
+  char numChar[MAX_LENGTH_INT + 1];
+  sprintf(numChar, "%d", num);
+  for (unsigned int i = 0; i < strlen(numChar); i++)
+    kernel->synchConsoleOut->PutChar(numChar[i]);
+  kernel->synchConsoleOut->PutChar('\n');
+}
+
+/**/
+int SysRandomNum()
+{
+  srandom(time(NULL));
+  return random();
+}
+
+/**/
+char SysReadChar()
+{
+  return kernel->synchConsoleIn->GetChar();
+}
+
+/**/
+void SysPrintChar(char c)
+{
+  kernel->synchConsoleOut->PutChar(c);
+}
 
 #endif /* ! __USERPROG_KSYSCALL_H__ */
